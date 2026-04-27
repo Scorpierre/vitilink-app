69f1985aafebcae3c2ae33c59886f73b719d8263
Pierre <pierre.connes@epitech.eu>
2026-04-27 16:32:45 +0100

    add .env.example by services

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,25 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
-    // Configuration CORS
-    app.enableCors({
-      origin: 'http://localhost:5173',
-      credentials: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 200
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
   });
 
-  // Validation des données entrantes
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
-    forbidNonWhitelisted: true
+    forbidNonWhitelisted: true,
   }));
 
   await app.listen(process.env.PORT ?? 3000);
